1c6a21064647efd0363be407259fb8f7199764c1
Chencheng Fang <100428466+ccfang2@users.noreply.github.com>
2025-04-14 19:05:36 +0200

    Add files via upload

--- R/plot.fdid_scb.R
+++ R/plot.fdid_scb.R
@@ -5,9 +5,11 @@
 #' @param object an object of class \code{"fdid_scb"}. The object to be plotted.
 #' @param ta.t0 a numeric value that indicates the time, right after which there is an anticipation of treatment. It should be
 #' greater than the minimal event time and no greater than t0 in \code{object}. If it is NULL, there is no anticipation.
-#' @param frm.mbar a numeric value of tuning parameter for functional relative magnitudes.
-#' @param ftr.m a numeric value of tuning parameter for functional trend restrictions.
+#' @param ta.s a numeric value of tuning parameter for honest inference under treatment anticipation. If NULL, the critical value \eqn{t_{1-\alpha/2, df}} or \eqn{z_{1-\alpha/2}} is applied with the same \eqn{\alpha} used for constructing simultaneous confidence band. If no treatment anticipation is defined in \code{ta.t0}, the value of \code{ta.s} is ignored.
+#' @param frm.mbar a numeric value of tuning parameter for honest inference under functional relative magnitudes.
+#' @param ftr.m a numeric value of tuning parameter for honest inference under functional trend restrictions.
 #' @param frmtr.mbar a numeric value of tuning parameter for combining functional relative magnitudes and functional trend restrictions.
+#' @param post.trt a logical value. If TRUE, only bounds for post-treatment periods are plotted and used for honest inference.
 #' @param ci a logical value. If TRUE, the point-wise confidence intervals are also plotted.
 #' @param pos.legend a character value of "top" or "bottom" that indicates the position of legend. If NULL, the legend is not printed.
 #' @param ... Additional arguments to be passed to \link[base]{plot}.
@@ -33,9 +35,18 @@
 #' ## honest inference under treatment anticipation
 #' plot(fdid_scb_est, ta.t0=-4)
 #'
-#' ## honest inference under differential trend
+#' ## honest inference under violation of parallel trends
 #' plot(fdid_scb_est, frm.mbar=0.3)
-plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.mbar=NULL, ci=TRUE, pos.legend="top", ...) {
+plot.fdid_scb <- function(object,
+                          ta.t0=NULL,
+                          ta.s=NULL,
+                          frm.mbar=NULL,
+                          ftr.m=NULL,
+                          frmtr.mbar=NULL,
+                          post.trt=FALSE,
+                          ci=TRUE,
+                          pos.legend="top",
+                          ...) {
 
   # give out a warn
   warning_note <- paste("The inference result around the reference time t=", object$data$t0, "(i.e. between two event time closest to the reference time) should be treated with caution.")
@@ -45,14 +56,16 @@ plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.m
 
   # check conditions
   if (!base::inherits(object,"fdid_scb")) stop("The input 'object' should be an output of function 'fdid_scb'.")
-  if (!is.logical(ci)) stop("The input 'ci' should be logical.")
   if (!is.null(ta.t0) && (!is.numeric(ta.t0) || length(ta.t0) != 1)) stop("The input 'ta.t0' should be either NULL or a numeric scalar.")
   if (!is.null(ta.t0) && !(ta.t0 %in% object$scb$event_t[which(object$scb$event_t<=object$data$t0)])) stop("If not NULL, the input 'ta.t0' should be among the pre-treatment event time in 'object'.")
   if (!is.null(ta.t0) && ta.t0 == object$scb$event_t[1] && !all(sapply(list(frm.mbar, ftr.m, frmtr.mbar), is.null))) stop("If 'ta.t0' is defined to be the first event time, 'frm.mbar', 'ftr.m' and 'frmtr.mbar' must be NULL, because there is no available data for computing pre-trend differences.")
+  if (!is.null(ta.s) && (!is.numeric(ta.s) || length(ta.s) != 1 || ta.s < 0)) stop("The input 'ta.s' should be either NULL or a numeric non-negative scalar.")
   if (!is.null(frm.mbar) && (!is.numeric(frm.mbar) || length(frm.mbar) != 1 || frm.mbar < 0)) stop("The input 'frm.mbar' should be either NULL or a numeric non-negative scalar.")
   if (!is.null(ftr.m) && (!is.numeric(ftr.m) || length(ftr.m) != 1 || ftr.m < 0)) stop("The input 'ftr.m' should be either NULL or a numeric non-negative scalar.")
   if (!is.null(frmtr.mbar) && (!is.numeric(frmtr.mbar) || length(frmtr.mbar) != 1 || frmtr.mbar < 0)) stop("The input 'frmtr.mbar' should be either NULL or a numeric non-negative scalar.")
   if (!is.null(frm.mbar) && !is.null(ftr.m) && !is.null(frmtr.mbar)) stop("Exactly one of inputs 'frm.mbar', 'ftr.m' and 'frmtr.mbar' can have a value, or all three are NULL.")
+  if (!is.logical(post.trt)) stop("The input 'post.trt' should be logical.")
+  if (!is.logical(ci)) stop("The input 'ci' should be logical.")
   if (!is.null(pos.legend) && !pos.legend %in% c("top", "bottom")) stop("The input 'pos.legend' must be 'top', 'bottom', or NULL.")
 
   # extract data from object
@@ -70,8 +83,6 @@ plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.m
   start                     <- min(object$data$beta[,2])
   end                       <- max(object$data$beta[,2])
   t0                        <- object$data$t0
-  df                        <- object$data$df
-  conf.level                <- object$data$conf.level
   if (is.null(ta.t0)) ta.t0 <- t0
 
   # regular statistical inference
@@ -109,14 +120,27 @@ plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.m
     curve(betahat_splinefun, add=TRUE, lty=1)
     curve(scb_ub_splinefun, add=TRUE, lty=1)
     curve(scb_lb_splinefun, add=TRUE, lty=1)
-    abline(h=0, lty=3, lwd=4, col="red")
 
-    for (i in 1:(length(roots_vec)-1)) {
-      if (stat_sig_vec[i]==1) {
-        rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.7), border=NA)
-      } else {
-        if (stat_sig_vec[i]==-1) {
-          rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.4), border=NA)
+    if(isTRUE(post.trt)) {
+      segments(x0=t0, y0=0, x1=end, y1=0, lty=3, lwd=4, col="red")
+      for (i in 1:(length(roots_vec)-1)) {
+        if (stat_sig_vec[i]==1 & roots_vec[i]>=t0 ) {
+          rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.7), border=NA)
+        } else {
+          if (stat_sig_vec[i]==-1 & roots_vec[i]>=t0) {
+            rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.4), border=NA)
+          }
+        }
+      }
+    } else {
+      segments(x0=start, y0=0, x1=end, y1=0, lty=3, lwd=4, col="red")
+      for (i in 1:(length(roots_vec)-1)) {
+        if (stat_sig_vec[i]==1) {
+          rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.7), border=NA)
+        } else {
+          if (stat_sig_vec[i]==-1) {
+            rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.4), border=NA)
+          }
         }
       }
     }
@@ -156,14 +180,12 @@ plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.m
     # honest inference
     if(is.null(frm.mbar) & is.null(ftr.m) & is.null(frmtr.mbar)) {
 
-      if(ta.t0==t0) {ta.alpha <- 1} else {ta.alpha <- 1-conf.level}
-
-      if(is.null(df)) {
-        ta_ub <- betahat[which(timeVec==ta.t0)]+qnorm(p=1-ta.alpha/2)*sqrt(diag(covhat)[which(timeVec==ta.t0)]) #change here if focusing only on post-treatment periods
-        ta_lb <- betahat[which(timeVec==ta.t0)]-qnorm(p=1-ta.alpha/2)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+      if(is.null(ta.s)) {
+        ta_ub <- ci_upper[names(ci_upper)==as.character(ta.t0)]
+        ta_lb <- ci_lower[names(ci_lower)==as.character(ta.t0)]
       } else {
-        ta_ub <- betahat[which(timeVec==ta.t0)]+qt(p=1-ta.alpha/2,df=df)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
-        ta_lb <- betahat[which(timeVec==ta.t0)]-qt(p=1-ta.alpha/2,df=df)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+        ta_ub <- betahat[which(timeVec==ta.t0)]+ta.s*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+        ta_lb <- betahat[which(timeVec==ta.t0)]-ta.s*sqrt(diag(covhat)[which(timeVec==ta.t0)])
       }
 
       honest_ub_splinefun <- function(x) ta_ub
@@ -174,16 +196,15 @@ plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.m
 
     }
 
+
     if(!is.null(frm.mbar) & is.null(ftr.m) & is.null(frmtr.mbar)) {
 
-      if(ta.t0==t0) {ta.alpha <- 1} else {ta.alpha <- 1-conf.level}
-
-      if(is.null(df)) {
-        ta_ub <- betahat[which(timeVec==ta.t0)]+qnorm(p=1-ta.alpha/2)*sqrt(diag(covhat)[which(timeVec==ta.t0)]) #change here if focusing only on post-treatment/post-anticipation periods
-        ta_lb <- betahat[which(timeVec==ta.t0)]-qnorm(p=1-ta.alpha/2)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+      if(is.null(ta.s)) {
+        ta_ub <- ci_upper[names(ci_upper)==as.character(ta.t0)]
+        ta_lb <- ci_lower[names(ci_lower)==as.character(ta.t0)]
       } else {
-        ta_ub <- betahat[which(timeVec==ta.t0)]+qt(p=1-ta.alpha/2, df=df)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
-        ta_lb <- betahat[which(timeVec==ta.t0)]-qt(p=1-ta.alpha/2, df=df)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+        ta_ub <- betahat[which(timeVec==ta.t0)]+ta.s*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+        ta_lb <- betahat[which(timeVec==ta.t0)]-ta.s*sqrt(diag(covhat)[which(timeVec==ta.t0)])
       }
 
       # frm bounds
@@ -214,14 +235,12 @@ plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.m
 
     if(is.null(frm.mbar) & !is.null(ftr.m) & is.null(frmtr.mbar)) {
 
-      if(ta.t0==t0) {ta.alpha <- 1} else {ta.alpha <- 1-conf.level}
-
-      if(is.null(df)) {
-        ta_ub <- betahat[which(timeVec==ta.t0)]+qnorm(p=1-ta.alpha/2)*sqrt(diag(covhat)[which(timeVec==ta.t0)]) #change here if focusing only on post-treatment periods
-        ta_lb <- betahat[which(timeVec==ta.t0)]-qnorm(p=1-ta.alpha/2)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+      if(is.null(ta.s)) {
+        ta_ub <- ci_upper[names(ci_upper)==as.character(ta.t0)]
+        ta_lb <- ci_lower[names(ci_lower)==as.character(ta.t0)]
       } else {
-        ta_ub <- betahat[which(timeVec==ta.t0)]+qt(p=1-ta.alpha/2, df=df)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
-        ta_lb <- betahat[which(timeVec==ta.t0)]-qt(p=1-ta.alpha/2, df=df)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+        ta_ub <- betahat[which(timeVec==ta.t0)]+ta.s*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+        ta_lb <- betahat[which(timeVec==ta.t0)]-ta.s*sqrt(diag(covhat)[which(timeVec==ta.t0)])
       }
 
       # ftr bounds
@@ -251,14 +270,12 @@ plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.m
 
     if(is.null(frm.mbar) & is.null(ftr.m) & !is.null(frmtr.mbar)) {
 
-      if(ta.t0==t0) {ta.alpha <- 1} else {ta.alpha <- 1-conf.level}
-
-      if(is.null(df)) {
-        ta_ub <- betahat[which(timeVec==ta.t0)]+qnorm(p=1-ta.alpha/2)*sqrt(diag(covhat)[which(timeVec==ta.t0)]) #change here if focusing only on post-treatment periods
-        ta_lb <- betahat[which(timeVec==ta.t0)]-qnorm(p=1-ta.alpha/2)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+      if(is.null(ta.s)) {
+        ta_ub <- ci_upper[names(ci_upper)==as.character(ta.t0)]
+        ta_lb <- ci_lower[names(ci_lower)==as.character(ta.t0)]
       } else {
-        ta_ub <- betahat[which(timeVec==ta.t0)]+qt(p=1-ta.alpha/2, df=df)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
-        ta_lb <- betahat[which(timeVec==ta.t0)]-qt(p=1-ta.alpha/2, df=df)*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+        ta_ub <- betahat[which(timeVec==ta.t0)]+ta.s*sqrt(diag(covhat)[which(timeVec==ta.t0)])
+        ta_lb <- betahat[which(timeVec==ta.t0)]-ta.s*sqrt(diag(covhat)[which(timeVec==ta.t0)])
       }
 
       # frmtr bounds
@@ -293,7 +310,7 @@ plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.m
     roots_UB <- vector("numeric", length = n.int)
     for (i in 1:n.int) {
       fun         <- function(x) (scb_ub_splinefun(x)-honest_ub_splinefun(x))*(scb_lb_splinefun(x)-honest_ub_splinefun(x))
-      interval    <- c(start+(i-1)*(end-start)/n.int, start+(i)*(end-start)/n.int) #change here if focusing only on post-treatment/post-anticipation periods
+      interval    <- c(start+(i-1)*(end-start)/n.int, start+(i)*(end-start)/n.int)
       root        <- try(uniroot(fun, interval = interval)$root, silent = TRUE)
       roots_UB[i] <- if(inherits(root, "try-error")) {NA} else root
     }
@@ -361,17 +378,33 @@ plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.m
     curve(betahat_splinefun, add=TRUE, lty=1)
     curve(scb_ub_splinefun, add=TRUE, lty=1)
     curve(scb_lb_splinefun, add=TRUE, lty=1)
-    abline(h=0, lty=3, lwd=4, col="red")
 
     ShadeBetween <- function(x1, x2, f1, f2, ...) {polygon(c(x1, rev(x2)), c(f1, rev(f2)), ...)}
-    ShadeBetween(timeVec,timeVec, honest_ub_splinefun(timeVec) , honest_lb_splinefun(timeVec),  col = rgb(1, 0, 0, alpha = 0.3), border = NA)
 
-    for (i in 1:(length(roots_vec)-1) ) {
-      if ( fun_stat_sig_vec((roots_vec[i]+roots_vec[i+1])/2)==1  ) {
-        rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.7), border=NA)
-      } else {
-        if (fun_stat_sig_vec((roots_vec[i]+roots_vec[i+1])/2)==-1) {
-          rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.4), border=NA)
+    if(isTRUE(post.trt)) {
+      segments(x0=t0, y0=0, x1=end, y1=0, lty=3, lwd=4, col="red")
+      ShadeBetween(timeVec[timeVec>=t0], timeVec[timeVec>=t0], honest_ub_splinefun(timeVec[timeVec>=t0]), honest_lb_splinefun(timeVec[timeVec>=t0]), col=rgb(1,0,0,alpha=0.3), border=NA)
+
+      for (i in 1:(length(roots_vec)-1) ) {
+        if ( fun_stat_sig_vec((roots_vec[i]+roots_vec[i+1])/2)==1 & roots_vec[i]>=t0 ) {
+          rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.7), border=NA)
+        } else {
+          if (fun_stat_sig_vec((roots_vec[i]+roots_vec[i+1])/2)==-1 & roots_vec[i]>=t0 ) {
+            rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.4), border=NA)
+          }
+        }
+      }
+    } else {
+      segments(x0=start, y0=0, x1=end, y1=0, lty=3, lwd=4, col="red")
+      ShadeBetween(timeVec, timeVec, honest_ub_splinefun(timeVec), honest_lb_splinefun(timeVec), col=rgb(1,0,0,alpha=0.3), border=NA)
+
+      for (i in 1:(length(roots_vec)-1) ) {
+        if ( fun_stat_sig_vec((roots_vec[i]+roots_vec[i+1])/2)==1  ) {
+          rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.7), border=NA)
+        } else {
+          if (fun_stat_sig_vec((roots_vec[i]+roots_vec[i+1])/2)==-1) {
+            rect(roots_vec[i], y_range[1], roots_vec[i+1], y_range[2], col=rgb(0.5, 0.5, 0.5, alpha=0.4), border=NA)
+          }
         }
       }
     }
@@ -423,8 +456,3 @@ plot.fdid_scb <- function(object, ta.t0=NULL, frm.mbar=NULL, ftr.m=NULL, frmtr.m
   }
   options(warn=1)
 }
-
-
-
-
-
